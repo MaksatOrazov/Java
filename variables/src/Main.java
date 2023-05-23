26c783af5a894a9f53dce8aa3b4dcbdcb3110eda
MaksatOrazov <maxi.orazov1996@gmail.com>
2023-05-23 02:51:17 -0700

    variable

---
+++ variables/src/Main.java
@@ -0,0 +1,11 @@
+//reusability
+public class Main {
+    public static void main(String[] args) {
+        int studentNumber = 23;
+        String message = "Student number : ";
+        System.out.println(message + studentNumber);
+        System.out.println(message + studentNumber);
+        System.out.println(message + studentNumber);
+
+    }
+}
