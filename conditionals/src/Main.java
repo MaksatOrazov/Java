c05996f2e2d1555150429ddcd440cf8378829b15
MaksatOrazov <maxi.orazov1996@gmail.com>
2023-05-27 02:48:00 -0700

    Conditionals

---
+++ conditionals/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = 21;
+        if (number < 20){
+            System.out.println("The number is lower than 20");
+        }else if (number == 20){
+            System.out.println("The number is equal to 20");
+        }else {
+            System.out.println("The number is bigger than 20");
+        }
+    }
+}
