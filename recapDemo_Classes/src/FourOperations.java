a494f1b87e546eb090597e88290d5250e6c6d92d
MaksatOrazov <maxi.orazov1996@gmail.com>
2023-06-15 10:35:14 +0400

    recapDemo_classes

---
+++ recapDemo_Classes/src/FourOperations.java
@@ -0,0 +1,15 @@
+public class FourOperations {
+    public int Sum(int number1, int number2){
+        return number1 + number2;
+    }
+    public int Extraction(int number1, int number2){
+        return number1 - number2;
+    }
+    public int Multiplication(int number1, int number2){
+        return number1 * number2;
+    }
+    public int Divide(int number1, int number2){
+        return number1 / number2;
+    }
+
+}
---
+++ recapDemo_Classes/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        FourOperations fourOperations =new FourOperations();
+        int result = fourOperations.Sum(5,6);
+        System.out.println(result);
+    }
+}
