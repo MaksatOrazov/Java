e3b292cafefa71c27cb9a129304a08031fc3ea00
MaksatOrazov <maxi.orazov1996@gmail.com>
2023-06-26 12:28:23 +0400

    dataTypes/

---
+++ dataTypes/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = 12;
+        number = -1234;
+        char charakter = 'A';
+        boolean isItTrue = false;
+    }
+}
