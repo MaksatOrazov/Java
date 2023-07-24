161cff63e9d1c364594f87352514432905d8d215
MaksatOrazov <maxi.orazov1996@gmail.com>
2023-07-24 12:23:10 +0400

    methodsOverloading

---
+++ methodOverloading/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        FourOperations fourOperations = new FourOperations();
+        System.out.println(fourOperations.sum(3,4));
+        System.out.println(fourOperations.sum(3,4,6));
+
+    }
+}
